be4067fefe2d864009949bfcf5c091519968d273
hadiyaeb <hadiya.ibrahim394@gmail.com>
2024-01-24 12:40:31 +0500

    commit2

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const app = express();
 const { MongoClient } = require('mongodb');
 
+const app = express();
 app.use(express.json());
 
 // Replace <username>, <password>, and <dbname> with your MongoDB Atlas username, password, and database name
 const uri = "mongodb+srv://hadiyaebrahim:Hello123@cluster0.cojugwv.mongodb.net/?retryWrites=true&w=majority";
 
-
 async function connectToMongoDB() {
   try {
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
     let isConnected = false;
 
     await client.connect();
@@ -21,22 +20,22 @@ async function connectToMongoDB() {
     async function handlePostRequest(req, res) {
       try {
         console.log(req.body);
-    
+
         if (!isConnected) {
           throw new Error("MongoDB client is not connected");
         }
-    
+
         const {
           transmitterSerialNumber,
           nodeType,
           reads,
           allCount
         } = req.body;
-    
-        if (!transmitterSerialNumber || !nodeType || !reads || !Array.isArray(reads)) {
+
+        if (!transmitterSerialNumber || !nodeType || !reads || !Array.isArray(reads) || !allCount) {
           throw new Error("Invalid request body format");
         }
-    
+
         for (const read of reads) {
           const {
             timeStampUTC,
@@ -45,12 +44,12 @@ async function connectToMongoDB() {
             distance,
             count
           } = read;
-    
+
           if (!timeStampUTC || !deviceUID || !manufacturerName || !distance || !count) {
             console.error("Invalid read information:", read);
             continue; // Skip to the next iteration if the read information is incomplete
           }
-    
+
           const dataWithTimestamp = {
             transmitterSerialNumber,
             nodeType,
@@ -60,28 +59,28 @@ async function connectToMongoDB() {
             distance,
             count,
           };
-    
+
           const result = await collection.insertOne(dataWithTimestamp);
           console.log("1 document inserted");
         }
-    
+
         // Additional data with allCount
         const additionalData = {
           transmitterSerialNumber,
           nodeType,
           allCount
         };
-    
+
         const result = await collection.insertOne(additionalData);
         console.log("Additional document inserted");
-    
+
         res.sendStatus(200);
       } catch (err) {
         console.error("Error inserting documents:", err);
         res.status(500).send("Internal Server Error: " + err.message);
       }
     }
-    
+
     app.post('/data', handlePostRequest);
 
     app.listen(8080, () => console.log('Server listening on port 8080'));
